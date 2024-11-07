adccec8c476503218386c9965e2bcd3745e42ed6
Schmartovich <saf9143@thi.de>
2024-11-07 04:34:17 +0100

    window can be opened

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
+
+    // Screen settings
     final int originalTileSize = 16; // 16x16 px
     final int scale = 3;
     final int tileSize = originalTileSize * scale;
 
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
+    final int screenWidth = maxScreenCol * tileSize;
+    final int screenHeight = maxScreenRow * tileSize;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true); // draw offscreen
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("THE GAME TITEL");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack(); //size to preferred size
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
