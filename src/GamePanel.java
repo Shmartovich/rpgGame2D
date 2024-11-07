207db5fcc9011241e2057fb233ae547ba20bc871
Schmartovich <saf9143@thi.de>
2024-11-07 05:02:56 +0100

    blue rectangle on the window is there

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // Screen settings
     final int originalTileSize = 16; // 16x16 px
@@ -13,9 +13,35 @@ public class GamePanel extends JPanel {
     final int screenWidth = maxScreenCol * tileSize;
     final int screenHeight = maxScreenRow * tileSize;
 
+    Thread gameThread;
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); // draw offscreen
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+        //update
+        update();
+
+        //draw
+        repaint();
+    }
+    public void update() {
+
+    }
+    public void paintComponent(Graphics g) { // from JComponent
+        super.paintComponent(g); // parent is JPanel
+
+        Graphics2D g2 = (Graphics2D) g; // subclass of Graphics that is used for drawing
+        g2.setColor(Color.blue);
+        g2.fillRect(10, 10, tileSize, tileSize);
+        g2.dispose(); // optimisation
     }
 }
