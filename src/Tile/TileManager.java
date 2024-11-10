93bf162400ab3d67589bcc574085772068641d21
Schmartovich <saf9143@thi.de>
2024-11-10 03:11:25 +0100

    not really functional load map from csv

--- src/Tile/TileManager.java
+++ src/Tile/TileManager.java
@@ -27,19 +27,26 @@ public class TileManager {
     public void draw(Graphics2D graphics2D){
         int playerCol = playerX / gamePanel.tileSize;
         int playerRow = playerY / gamePanel.tileSize;
-        int col;
-        int row;
-
-            //todo i,j are from which point?! player is a center these are "edges"
-            int tileID = int2DimList.get(playerCol).get(playerRow);
-            String tilePath = String.format("/tilesNumbered/%d.png", tileID);
-            try {
-                BufferedImage image = ImageIO.read(getClass().getResource(tilePath));
-                graphics2D.drawImage(image, playerCol, playerCol, gamePanel.tileSize,gamePanel.tileSize,null);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        int col = 0;
+        int row = 0;
+        int tileID;
+        for (int i = playerRow - 1; i <= playerRow + 1; i++) {
+            for (int j = playerCol - 1; j <= playerCol + 1; j++) {
+                // Check constraints
+                if (i >= 0 && i < int2DimList.size() && j >= 0 && j < int2DimList.get(0).size()) { //todo be cautious with 0
+                    tileID = int2DimList.get(i).get(j);
+                    String tilePath = String.format("/tilesNumbered/%d.png", tileID);
+                    try {
+                        BufferedImage image = ImageIO.read(getClass().getResource(tilePath));
+                        graphics2D.drawImage(image, col * gamePanel.tileSize, row * gamePanel.tileSize, gamePanel.tileSize, gamePanel.tileSize, null);
+                        col++;
+                        row++;
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
-
+        }
     }
 
     private void loadMap(String path){
