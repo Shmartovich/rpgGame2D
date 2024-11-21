b07d0d5c0f44794ebd79540b53e9efea152c3cdc
Schmartovich <saf9143@thi.de>
2024-11-21 14:49:38 +0100

    minor changes collision checker

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -17,45 +17,51 @@ public class Player extends Entity {
     //todo rename player*
     final int playerFrameWidth = 16; // for correct cut of frames sheet
     final int playerFrameHeight = 24;
+    final int playerWidth;
+    final int playerHeight;
     final int playerAnimationRunFrames = 24;
     Animation animationAdam = new Animation("/Player/adam_run.png", playerFrameWidth, playerFrameHeight, playerAnimationRunFrames);
-    public Player(GamePanel gp, KeyHandler kh){
+
+    public Player(GamePanel gp, KeyHandler kh) {
         gamePanel = gp;
+        playerWidth = playerFrameWidth * gamePanel.scale;
+        playerHeight = playerFrameHeight * gamePanel.scale;
         keyHandler = kh;
         setDefaultValues();
     }
-    public void setDefaultValues(){
+
+    public void setDefaultValues() {
         screenCenterX = gamePanel.screenWidth / 2 - gamePanel.tileSize / 2;
         screenCenterY = gamePanel.screenHeight / 2 - gamePanel.tileSize / 2;
         worldX = 48;
         worldY = 48;
         collisionRectangle = new Rectangle();
-        collisionRectangle.x = 8;
-        collisionRectangle.y = 16;
-        collisionRectangle.width = playerFrameWidth * gamePanel.scale - 16;
-        collisionRectangle.height = playerFrameHeight * gamePanel.scale - 16;
+        collisionRectangle.x = 10;
+        collisionRectangle.y = 24;
+        collisionRectangle.width = playerWidth - collisionRectangle.x;
+        collisionRectangle.height = playerHeight - collisionRectangle.y;
         speed = 4;
     }
 
     public void update(KeyHandler keyHandler) {
-        if(keyHandler.downPressed || keyHandler.leftPressed || keyHandler.rightPressed || keyHandler.upPressed){
-            if(keyHandler.upPressed){
+        if (keyHandler.downPressed || keyHandler.leftPressed || keyHandler.rightPressed || keyHandler.upPressed) {
+            if (keyHandler.upPressed) {
                 direction = "up";
-            }else if(keyHandler.downPressed){
+            } else if (keyHandler.downPressed) {
                 direction = "down";
-            }else if(keyHandler.leftPressed){
+            } else if (keyHandler.leftPressed) {
                 direction = "left";
-            }else if(keyHandler.rightPressed){
+            } else if (keyHandler.rightPressed) {
                 direction = "right";
             }
 
             // ACTIVATE COLLISION ON PLAYER IF ON SOLID TILE
             collisionOn = false;
-            System.out.printf("ich stehe hier: col %d, row %d\n", worldX/ gamePanel.tileSize, worldY/ gamePanel.tileSize);
-            printCollisionCoordinates();
+            System.out.printf("ich stehe hier: col %d, row %d\n", worldX / gamePanel.tileSize, worldY / gamePanel.tileSize);
+            printPlayerCoordinates();
             gamePanel.collisionChecker.checkTile(this);
-            if(!collisionOn){
-                switch (direction){
+            if (!collisionOn) {
+                switch (direction) {
                     case "up":
                         worldY -= speed;
                         break;
@@ -75,26 +81,25 @@ public class Player extends Entity {
 
             //todo soll ich die animation nicht abspielen wenn player sich nicht bewegen kann?
             timerToChangeSprite++;
-            if(timerToChangeSprite > 10){
+            if (timerToChangeSprite > 10) {
                 timerToChangeSprite = 0;
-                if (spriteNumber == 5){
+                if (spriteNumber == 5) {
                     spriteNumber = 0;
                 }
                 spriteNumber++;
             }
-        }
-        else{
+        } else {
             //todo idle animation
             spriteNumber = 0;
         }
 
     }
 
-    public void draw(Graphics2D g2){
+    public void draw(Graphics2D g2) {
         BufferedImage imageToDraw = animationAdam.getFrames()[0];
         final int imagesInSheet = 6; // animation of running has 6 frames to each direction
 
-        switch (direction){
+        switch (direction) {
             case "up":
                 directionIndex = 1;
                 break;
@@ -114,16 +119,11 @@ public class Player extends Entity {
         g2.drawImage(imageToDraw, screenCenterX, screenCenterY, gamePanel.tileSize, playerFrameHeight * gamePanel.scale, null);
     }
 
-    private void printCollisionCoordinates(){
+    private void printPlayerCoordinates() {
         System.out.printf("Player coordinates: x-%d,y-%d,x+width-%d,y+height-%d\n",
                 this.worldX,
                 this.worldY,
                 this.worldX + playerFrameWidth * gamePanel.scale,
                 this.worldY + playerFrameHeight * gamePanel.scale);
-        System.out.printf("Collision coordinates: x-%d,y-%d,x+width-%d,y+height-%d\n",
-                this.collisionRectangle.x + worldX,
-                this.collisionRectangle.y + worldY,
-                this.collisionRectangle.x + worldX + collisionRectangle.width,
-                this.collisionRectangle.y + worldY + collisionRectangle.height);
     }
 }
--- src/Main/CollisionChecker.java
+++ src/Main/CollisionChecker.java
@@ -18,15 +18,16 @@ public class CollisionChecker {
         int collisionTopY = entity.worldY + entity.collisionRectangle.y;
         int collisionBottomY = collisionTopY + entity.collisionRectangle.height;
 
-        int collisionTopRow = (collisionTopY + entity.speed) / gamePanel.tileSize;
+        int collisionTopRow = (collisionTopY) / gamePanel.tileSize;
         int collisionBottomRow = (collisionBottomY) / gamePanel.tileSize;
-        int collisionLeftCol = (collisionLeftX + entity.speed) / gamePanel.tileSize;
-        int collisionRightCol =  (collisionRightX + entity.speed) / gamePanel.tileSize;
+        int collisionLeftCol = (collisionLeftX ) / gamePanel.tileSize;
+        int collisionRightCol =  (collisionRightX ) / gamePanel.tileSize;
 
         int tile1_ID;
         int tile2_ID;
         switch(entity.direction){
             case "up":
+                collisionTopRow = (collisionTopY - entity.speed) / gamePanel.tileSize;
                 tile1_ID = gamePanel.tileManager.parsedMap.get(collisionTopRow).get(collisionLeftCol);
                 tile2_ID = gamePanel.tileManager.parsedMap.get(collisionTopRow).get(collisionRightCol);
                 if(tileHasCollision(TileManager.tiles, tile1_ID) || tileHasCollision(TileManager.tiles, tile2_ID)){
@@ -36,6 +37,7 @@ public class CollisionChecker {
                 }
                 break;
             case "down":
+                collisionBottomRow = (collisionBottomY) / gamePanel.tileSize;
                 tile1_ID = gamePanel.tileManager.parsedMap.get(collisionBottomRow).get(collisionLeftCol);
                 tile2_ID = gamePanel.tileManager.parsedMap.get(collisionBottomRow).get(collisionRightCol);
                 if(tileHasCollision(TileManager.tiles, tile1_ID) || tileHasCollision(TileManager.tiles, tile2_ID)){
@@ -45,6 +47,7 @@ public class CollisionChecker {
                 }
                 break;
             case "left":
+                collisionLeftCol = (collisionLeftX - entity.speed) / gamePanel.tileSize;
                 tile1_ID = gamePanel.tileManager.parsedMap.get(collisionTopRow).get(collisionLeftCol);
                 tile2_ID = gamePanel.tileManager.parsedMap.get(collisionBottomRow).get(collisionLeftCol);
                 if(tileHasCollision(TileManager.tiles, tile1_ID) || tileHasCollision(TileManager.tiles, tile2_ID)){
@@ -54,6 +57,7 @@ public class CollisionChecker {
                 }
                 break;
             case "right":
+                collisionRightCol = (collisionRightX + entity.speed) / gamePanel.tileSize;
                 tile1_ID = gamePanel.tileManager.parsedMap.get(collisionTopRow).get(collisionRightCol);
                 tile2_ID = gamePanel.tileManager.parsedMap.get(collisionBottomRow).get(collisionRightCol);
                 if(tileHasCollision(TileManager.tiles, tile1_ID) || tileHasCollision(TileManager.tiles, tile2_ID)){
